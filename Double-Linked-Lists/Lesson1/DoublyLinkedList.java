955af4fd0ab67e466ecf959ec3085f10bce98a86
Yunus Emre YAZICI <ynsmryzc46@gmail.com>
2022-07-14 09:50:23 +0300

    update

--- Double-Linked-Lists/Lesson1/DoublyLinkedList.java
+++ Double-Linked-Lists/Lesson1/DoublyLinkedList.java
@@ -1,3 +1,5 @@
+import java.util.NoSuchElementException;
+
 public class DoublyLinkedList {
 
     private ListNode head;
@@ -12,7 +14,6 @@ public class DoublyLinkedList {
         public ListNode(int data) {
             this.data = data;
         }
-
     }
 
     public DoublyLinkedList() {
@@ -23,23 +24,10 @@ public class DoublyLinkedList {
 
     public boolean isEmpty() {
         return length == 0; // head=null
-
     }
 
     public int length() {
         return length;
-    }
-
-    public void insertLast(int value){
-        ListNode newNode = new ListNode(value);
-        if (isEmpty()) {
-            head=newNode;
-        }else{
-            tail.next=newNode;
-        }
-        newNode.previous=tail;
-        tail=newNode;
-        length++;
     }
 
     public void displayForward() {
@@ -64,17 +52,43 @@ public class DoublyLinkedList {
             temp = temp.previous;
         }
         System.out.println("null");
-
     }
 
-    public static void main(String[] args) {
-
-        DoublyLinkedList dll = new DoublyLinkedList();
-        dll.insertLast(1);
-        dll.insertLast(10);
-        dll.insertLast(15);
-        dll.insertLast(25);
-        dll.displayForward();
-        dll.displayBackward();
+    public void insertFirst(int value) {
+        ListNode newNode = new ListNode(value);
+        if (isEmpty()) {
+            tail = newNode;
+        } else {
+            head.previous = newNode;
+        }
+        newNode.next = head;
+        head = newNode;
+        length++;
+    }
+    public void insertLast(int value) {
+        ListNode newNode = new ListNode(value);
+        if (isEmpty()) {
+            head = newNode;
+        } else {
+            tail.next = newNode;  
+            newNode.previous = tail;
+        }
+        tail = newNode;
+        length++;
+    }
+    public ListNode deleteFirst(){
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+        ListNode temp=head;
+        if (head==tail) {
+            tail= null;
+        }else{
+            head.next.previous= null;
+        }
+        head=head.next;
+        temp.next=null;
+        length--;
+        return temp;
     }
 }
---
+++ Double-Linked-Lists/Lesson1/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        DoublyLinkedList dll = new DoublyLinkedList();
+         dll.insertLast(1);
+         dll.insertLast(10);
+         dll.insertLast(15);
+        // dll.insertLast(25);
+        // dll.displayForward();
+        // dll.displayBackward();
+        // dll.insertFirst(1);
+        // dll.insertFirst(10);
+        dll.displayForward();
+        dll.deleteFirst();
+        dll.deleteFirst();
+        // dll.displayBackward();
+        dll.displayForward();
+    }
+}
